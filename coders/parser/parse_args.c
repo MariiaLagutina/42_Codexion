4edaf7949decc514ad5e075cf0d8ac4de3bff1e0
Mariia Lagutina <mlagutin@3-A-5.42heilbronn.de>
2026-04-20 18:54:36 +0200

    try to use everywhere POSIX not boolean

--- coders/parser/parse_args.c
+++ coders/parser/parse_args.c
@@ -6,7 +6,7 @@
 /*   By: mlagutin <mlagutin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 11:57:48 by mlagutin          #+#    #+#             */
-/*   Updated: 2026/04/20 13:31:13 by mlagutin         ###   ########.fr       */
+/*   Updated: 2026/04/20 18:25:38 by mlagutin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,11 @@
 static int	parse_scheduler(char *arg, t_scheduler *scheduler)
 {
 	if (!strcmp(arg, "fifo"))
-	{
 		*scheduler = FIFO;
-		return (1);
-	}
 	else if (!strcmp(arg, "edf"))
-	{
 		*scheduler = EDF;
+	else
 		return (1);
-	}
 	return (0);
 }
 
@@ -46,16 +42,16 @@ static int	parse_int_arg(const char *arg, int *out)
 	long	val;
 
 	if (!ft_strtol(arg, &val))
-		return (0);
+		return (1);
 	if (val > INT_MAX || val < INT_MIN)
-		return (0);
+		return (1);
 	*out = (int)val;
-	return (1);
+	return (0);
 }
 
 static int	parse_numbers(t_sim *sim, char **argv)
 {
-	if (!parse_int_arg(argv[1], &sim->number_of_coders))
+	if (parse_int_arg(argv[1], &sim->number_of_coders))
 		return (1);
 	if (!ft_strtol(argv[2], &sim->time_to_burnout))
 		return (1);
@@ -65,7 +61,7 @@ static int	parse_numbers(t_sim *sim, char **argv)
 		return (1);
 	if (!ft_strtol(argv[5], &sim->time_to_refactor))
 		return (1);
-	if (!parse_int_arg(argv[6], &sim->required_compiles))
+	if (parse_int_arg(argv[6], &sim->required_compiles))
 		return (1);
 	if (!ft_strtol(argv[7], &sim->dongle_cooldown))
 		return (1);
@@ -80,7 +76,7 @@ int	parse_arguments(t_sim *sim, int argc, char **argv)
 		return (1);
 	if (validate_values(sim))
 		return (1);
-	if (!parse_scheduler(argv[8], &sim->scheduler))
+	if (parse_scheduler(argv[8], &sim->scheduler))
 		return (1);
 	return (0);
 }
